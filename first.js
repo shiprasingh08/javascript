388a13111a9ae85575c37c71bb758fe3b15ce81f
shiprasingh08 <shiprasinghmau8@gmail.com>
2025-06-24 23:44:46 +0530

    ..

--- first.js
+++ first.js
@@ -1,6 +1,16 @@
-fullName = "Tony Stark";
+/*fullName = "Tony Stark";
 age = 45;
 price = 1445.49
 x = null;
 y = undefined;
-console.log(y);
+isFollow = true;
+isFollow = false;
+console.log(isFollow);*/
+
+
+/*let fullName = "Tony Stark";
+let age = 45;
+let price = 1445.49;
+
+console.log(fullName);*/
+
