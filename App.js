e05ef81cfe5f2061d68e938226549e48d63d85f1
shiprasingh08 <shiprasinghmau8@gmail.com>
2025-06-24 23:13:50 +0530

    ..

---
+++ App.js
@@ -0,0 +1,15 @@
+// a = 12;
+// console.log(a);
+
+//sum of two numbers
+//let a = 10;
+//let b = 20;
+//console.log(a+b);
+
+// if we add 1 number to a string, it will be called concattionation
+// sum and message
+//a = "10";
+//b = "20";
+//console.log(a+b+" is the sum of 10 and 20");
+
+//type of coersion
---
+++ first.js
@@ -0,0 +1,6 @@
+fullName = "Tony Stark";
+age = 45;
+price = 1445.49
+x = null;
+y = undefined;
+console.log(y);
