c607dc022723cc4a61c134967c4be5781bb5ba6e
shiprasingh08 <shiprasinghmau8@gmail.com>
2025-06-27 00:26:37 +0530

    ..

---
+++ airth.js
@@ -0,0 +1,27 @@
+// Airthmetic operators
+let a = 5;
+let b = 2;
+/* console.log( "a + b =", a +b);
+console.log( "a - b =", a - b);
+console.log( "a * b =", a * b);
+console.log( "a / b =", a / b);
+
+console.log( "a % b =", a % b);
+console.log( "a ** b =", a ** b);
+
+//unary operators
+console.log( "a++ =", a++);
+console.log( "a-- =", a--);*/
+
+
+//asssignment operators
+a += 4;
+console.log("a + 4 =", a);
+b -= 1;
+console.log("b - 1 =", b);
+
+a *= 2;
+console.log("a * 2 =", a);
+
+b /= 2;
+console.log("b /=", b);
---
+++ variable.js
@@ -0,0 +1,12 @@
+// Q-1
+
+let a ="shipra";
+let b= 6;
+console.log(a+b);
+
+//Q-2
+
+console.log(typeof (a+b));
+
+//Q-3
+ 
